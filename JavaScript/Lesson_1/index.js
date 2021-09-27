2e5e610290905c0eb7abd3d8d5681614ada128bb
Никита Кораблев <kit06092003@gmail.com>
2021-09-27 13:23:44 +0300

    not first commit

---
+++ JavaScript/Lesson_1/index.js
@@ -0,0 +1,22 @@
+console.log("Hello world!");
+
+const year = 2021;
+console.log(year);
+
+let planetName = "earth";
+let currentName = "somthing";
+let paperCount = "many";
+let isCashPay = true;
+
+console.log(planetName);
+console.log(currentName);
+console.log(paperCount);
+console.log(isCashPay);
+
+let firstName = "Nikita";
+let secondName = "Korablev";
+
+console.log(firstName);
+console.log(secondName);
+
+console.log(typeof firstName);
