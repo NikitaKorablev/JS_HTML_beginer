611df6bc09626ca120dabca3119d6f71cdbd3f75
Никита Кораблев <kit06092003@gmail.com>
2021-10-08 23:15:01 +0300

    new commit

---
+++ JavaScript/Lesson_2/Class_work/index.js
@@ -0,0 +1,16 @@
+console.log(5 > 4);
+console.log("ананас" > "яблоко");
+console.log("2" > "12");
+console.log(undefined == null);
+console.log(undefined === null);
+console.log(nul == "\n0\n");
+console.log(null ===+ "\n0\n");
+console.log("" + 1 + 0);
+console.log("" - 1 + 0);
+console.log(true + false);
+console.log(6 / "3");
+
+
+
+
+
